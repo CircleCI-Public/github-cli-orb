b55ae48c491e4928b5bd0f03fbf42a11f97021ed
Kyle Tryon <Kyle.Tryon@outlook.com>
2021-05-05 14:19:57 -0400

    fix pathing

--- src/scripts/setup.sh
+++ src/scripts/setup.sh
@@ -25,12 +25,12 @@ if ! command -v gh >/dev/null 2>&1; then
 			;;
 		macos)
 			curl -sSL "https://github.com/cli/cli/releases/download/v${PARAM_GH_CLI_VERSION}/gh_${PARAM_GH_CLI_VERSION}_macOS_amd64.tar.gz" -o "gh-cli.pkg"
-			$SUDO installer -pkg gh-cli.pkg -target /
+			$SUDO installer -pkg ./gh-cli.pkg -target /
 			rm gh-cli.pkg
 			;;
 		linux_arm)
 			curl -sSL "https://github.com/cli/cli/releases/download/v${PARAM_GH_CLI_VERSION}/gh_${PARAM_GH_CLI_VERSION}_linux_arm64.tar.gz" -o "gh-cli.tar.gz"
-			tar -xf gh-cli.tar.gz -C /usr/local/
+			tar -xf ./gh-cli.tar.gz -C /usr/local/
 			;;
 		*)
 		echo "This orb does not currently support your platform. If you believe it should, please consider opening an issue on the GitHub repository:"
