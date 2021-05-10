a70974367fc601bacb9b1ebe8d11c795196d23f9
Kyle Tryon <Kyle.Tryon@outlook.com>
2021-05-10 15:45:06 -0400

    more expansion debugging

--- src/scripts/clone.sh
+++ src/scripts/clone.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
 # Ensure known hosts are entered. Required for Docker.
 mkdir -p ~/.ssh
 echo 'github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==' >> ~/.ssh/known_hosts
 # GITHUB_CLONE_URL=${!PARAM_GH_REPO}
 echo "DEBUG"
-echo PARAM_GH_REPO: "$PARAM_GH_REPO"
-echo Expanded: "eval echo $PARAM_GH_REPO"
-gh repo clone "$CIRCLE_REPOSITORY_URL" "$PARAM_GH_DIR"
+echo "PARAM_GH_REPO:"
+echo "$PARAM_GH_REPO"
+echo "Expanded:"
+echo "${!PARAM_GH_REPO}"
+gh repo clone "$PARAM_GH_REPO" "$PARAM_GH_DIR"
