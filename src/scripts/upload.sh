84af9a082c12abbd00ec92ddd0d256ace9d3f18e
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-12-15 11:35:11 -0800

    feat: include `upload` job

---
+++ src/scripts/upload.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+additional_args="$(eval printf '%s\\n' "$ORB_EVAL_ADDITIONAL_ARGS")"
+files="$(eval printf '%s\\n' "$ORB_EVAL_FILES")"
+hostname="$(eval printf '%s' "$ORB_EVAL_HOSTNAME")"
+tag="$(eval printf '%s' "$ORB_EVAL_TAG")"
+token="${!ORB_ENV_TOKEN}"
+
+[ -z "$token" ] && { >&2 printf '%s\n' "A GitHub token must be supplied" "Check the \"token\" parameter."; exit 1; }
+printf '%s\n' "export GITHUB_TOKEN=$token" >> "$BASH_ENV"
+[ -n "$hostname" ] && printf '%s\n' "export GITHUB_HOSTNAME=$hostname" >> "$BASH_ENV"
+
+# shellcheck disable=SC2086
+gh release create \
+  "$tag" \
+  $files \
+  --repo "$(git config --get remote.origin.url)" \
+  $additional_args
