ed41f377fb026f97d91eccec3858d695ad0eb53f
markus <markus@helmes.com>
2022-12-19 14:51:01 +0200

    [semver:minor] Implement Github CLI "pr merge" command

---
+++ src/scripts/pr-merge.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+branch="$(eval printf '%s' "$ORB_EVAL_BRANCH")"
+additional_args="$(eval printf '%s\\n' "$ORB_EVAL_ADDITIONAL_ARGS")"
+hostname="$(eval printf '%s' "$ORB_EVAL_HOSTNAME")"
+token="${!ORB_ENV_TOKEN}"
+
+[ -z "$token" ] && { >&2 printf '%s\n' "A GitHub token must be supplied" "Check the \"token\" parameter."; exit 1; }
+printf '%s\n' "export GITHUB_TOKEN=$token" >> "$BASH_ENV"
+[ -n "$hostname" ] && printf '%s\n' "export GITHUB_HOSTNAME=$hostname" >> "$BASH_ENV"
+
+set -x
+# shellcheck disable=SC2086
+gh pr merge \
+  $branch
+  --repo "$(git config --get remote.origin.url)" \
+  $additional_args
+set +x
