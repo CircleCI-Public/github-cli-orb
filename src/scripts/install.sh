b1d3d57fd1f410c67b26ce8ca63652780af68eae
Eric Ribeiro <eric.ribeiro@circleci.com>
2023-12-13 15:43:02 -0800

    refactor: remove curl verbosity

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -9,16 +9,21 @@ version_ge() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" == "$1";
 # Check if a version is less than or equal to a given version
 version_le() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" == "$1"; }
 
-# Detect platform and architecture
 detect_platform() {
   case "$(uname -s)-$(uname -m)" in
-  "Darwin-x86_64") echo "macos_x86" ;;
+  "Darwin-x86_64") echo "macos_amd64" ;;
   "Darwin-arm64") echo "macos_arm" ;;
   "Linux-x86_64") echo "linux_x86" ;;
   "Linux-aarch64") echo "linux_arm" ;;
   *) echo "unsupported" ;;
   esac
 }
+
+# If the GH CLI is already installed then exit.
+if command -v gh >/dev/null 2>&1; then
+  echo "GH CLI is already installed."
+  exit 0
+fi
 
 platform="$(detect_platform)"
 if [ "$platform" == "unsupported" ]; then
@@ -42,12 +47,9 @@ if [[ "$platform" == macos_* ]] && version_ge "$PARAM_GH_CLI_VERSION" "2.28.0"; 
 fi
 
 case "$platform" in
-macos_x86)
-  download_url="https://github.com/cli/cli/releases/download/v${PARAM_GH_CLI_VERSION}/gh_${PARAM_GH_CLI_VERSION}_macOS_x86_64.$file_extension"
-  install_command="$sudo tar -xf ./gh-cli.$file_extension -C /usr/local/ --strip-components=1"
-  ;;
-macos_arm)
-  download_url="https://github.com/cli/cli/releases/download/v${PARAM_GH_CLI_VERSION}/gh_${PARAM_GH_CLI_VERSION}_macOS_arm64.$file_extension"
+macos_amd64 | macos_arm)
+  arch="${platform/macos_/}"
+  download_url="https://github.com/cli/cli/releases/download/v${PARAM_GH_CLI_VERSION}/gh_${PARAM_GH_CLI_VERSION}_macOS_${arch}.$file_extension"
   install_command="$sudo tar -xf ./gh-cli.$file_extension -C /usr/local/ --strip-components=1"
   ;;
 linux_x86)
@@ -59,29 +61,23 @@ linux_arm)
   download_url="https://github.com/cli/cli/releases/download/v${PARAM_GH_CLI_VERSION}/gh_${PARAM_GH_CLI_VERSION}_linux_arm64.tar.gz"
   install_command="$sudo tar -xf ./gh-cli.tar.gz -C /usr/local/ --strip-components=1"
   ;;
-*)
-  echo "This orb does not currently support your platform. If you believe it should, please consider opening an issue on the GitHub repository:"
-  echo "https://github.com/CircleCI-Public/github-cli-orb"
-  exit 1
-  ;;
 esac
 
-if command -v gh >/dev/null 2>&1; then
-  echo "GH CLI is already installed."
-  exit 0
-fi
-
 echo "Downloading the GitHub CLI from \"$download_url\"..."
-if ! curl -v -sSL "$download_url" -o "gh-cli.$file_extension"; then
+if ! curl -sSL "$download_url" -o "gh-cli.$file_extension"; then
   echo "Failed to download GH CLI from $download_url"
   exit 1
 fi
 
 echo "Installing the GitHub CLI..."
 set -x; $install_command; set +x
-
 echo "Cleaning up..."
 rm "gh-cli.$file_extension"
 
-echo "GH CLI installed"
-command -v gh
+if ! command -v gh >/dev/null 2>&1; then
+  echo "Something went wrong installing the GH CLI. Please try again or open an issue."
+  exit 1
+else
+  echo "GH CLI installed successfully"
+  command -v gh
+fi
