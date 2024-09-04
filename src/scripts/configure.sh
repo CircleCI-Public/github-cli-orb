738a7f60fe234369909c6eb0035a7f0dab10cfe4
Justin Burr <juburr@users.noreply.github.com>
2024-09-04 04:53:56 +0000

    explicit gh auth login to support self-hosted ghes

--- src/scripts/configure.sh
+++ src/scripts/configure.sh
@@ -7,6 +7,11 @@ echo "export GITHUB_TOKEN=\"${GITHUB_TOKEN}\"" >>"$BASH_ENV"
 # Authenticate
 echo
 echo "Authenticating GH CLI"
+echo "$GITHUB_TOKEN" | gh auth login --hostname "$PARAM_GH_HOSTNAME" --with-token
+
+# Setup git with GH CLI
+echo
+echo "Setting up git with GH CLI"
 gh auth setup-git --hostname "$PARAM_GH_HOSTNAME"
 
 echo
