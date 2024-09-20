afd04735ccc82dc2339b84f8334ef4851de81d64
Justin Burr <juburr@users.noreply.github.com>
2024-09-20 04:45:27 +0000

    remove gh auth login in favor of using GH_ENTERPRISE_TOKEN

--- src/scripts/configure.sh
+++ src/scripts/configure.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 # Get auth token
-export GITHUB_TOKEN=${!PARAM_GH_TOKEN}
-[ -z "$GITHUB_TOKEN" ] && echo "A GitHub token must be supplied. Check the \"token\" parameter." && exit 1
-echo "export GITHUB_TOKEN=\"${GITHUB_TOKEN}\"" >>"$BASH_ENV"
+[ -z "${!PARAM_GH_TOKEN}" ] && echo "A GitHub token must be supplied. Check the \"token\" parameter." && exit 1
+export GH_HOST=${!PARAM_GH_HOSTNAME}
+echo "export GH_HOST=\"${GH_HOST}\"" >> "$BASH_ENV"
 
-# Authenticate
-echo
-echo "Authenticating GH CLI"
-echo "$GITHUB_TOKEN" | gh auth login --hostname "$PARAM_GH_HOSTNAME" --with-token
+if [[ "${GH_HOST}" == "github.com" ]]; then
+    export GITHUB_TOKEN=${!PARAM_GH_TOKEN}
+    echo "export GITHUB_TOKEN=\"${GITHUB_TOKEN}\"" >> "$BASH_ENV"
+else
+    export GH_ENTERPRISE_TOKEN=${!PARAM_GH_TOKEN}
+    echo "export GH_ENTERPRISE_TOKEN=\"${GH_ENTERPRISE_TOKEN}\"" >> "$BASH_ENV"
+fi
 
 # Setup git with GH CLI
 echo
 echo "Setting up git with GH CLI"
-gh auth setup-git --hostname "$PARAM_GH_HOSTNAME"
+gh auth setup-git --hostname "$GH_HOST"
 
 echo
 echo "Viewing authentication GH authentication status"
