9918b5412b097c982d7cbab990180c7e49d99922
Mateo Arboleda <mateo.arboleda@circleci.com>
2024-11-25 16:12:42 -0500

    Update command to clone

--- src/scripts/clone.sh
+++ src/scripts/clone.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Ensure known hosts are entered. Required for Docker.
-set -x
 mkdir -p ~/.ssh
 
 ssh-keyscan -t rsa "$PARAM_GH_HOSTNAME" >> ~/.ssh/known_hosts
@@ -8,8 +7,8 @@ ssh-keyscan -t rsa "$PARAM_GH_HOSTNAME" >> ~/.ssh/known_hosts
 PARAM_GITHUB_REPO_EXPANDED="$(eval echo "$PARAM_GH_REPO")"
 
 if [ -n "$PARAM_BRANCH" ]; then
-    PARAM_BRANCH="-- --branch=$PARAM_BRANCH"
+    gh repo clone "$PARAM_GITHUB_REPO_EXPANDED" "$PARAM_GH_DIR" -- "--branch=$PARAM_BRANCH"
+else
+    gh repo clone "$PARAM_GITHUB_REPO_EXPANDED" "$PARAM_GH_DIR"
 fi
-echo "$PARAM_BRANCH"
-gh repo clone "$PARAM_GITHUB_REPO_EXPANDED" "$PARAM_GH_DIR" "$PARAM_BRANCH"
-set +x
+
