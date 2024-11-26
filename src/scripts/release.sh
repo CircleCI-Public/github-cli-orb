f534c737c7d31965926e6188db99351f96b59ed9
Mateo Arboleda <mateo.arboleda@circleci.com>
2024-11-26 14:20:24 -0500

    Change order of params

--- src/scripts/release.sh
+++ src/scripts/release.sh
@@ -11,6 +11,7 @@ if [ "$PARAM_GH_HOSTNAME" == 1 ]; then
 	export GITHUB_HOSTNAME=${!PARAM_GH_HOSTNAME}
 	echo "export GITHUB_HOSTNAME=\"${PARAM_GH_HOSTNAME}\"" >>"$BASH_ENV"
 fi
+set -- "$@" --repo "$(git config --get remote.origin.url)"
 
 if [ "$PARAM_GH_DRAFT" == 1 ]; then
 	set -- "$@" --draft
@@ -27,9 +28,7 @@ fi
 if [ -n "$PARAM_GH_FILES" ]; then
 	set -- "$@" $PARAM_GH_FILES
 fi
- 
-set -- "$@" --repo "$(git config --get remote.origin.url)"
-echo "$@"
+  
 # shellcheck disable=SC2086
 gh release create \
   "$PARAM_GH_TAG" \
